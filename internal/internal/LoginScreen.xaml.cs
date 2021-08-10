ef22871bb8547902f83ffe26b19a03f0a11974c4
John Doe <admirals16638@gmail.com>
2021-08-10 19:08:02 +1200

    Adding functionality to the submit button and connecting it to my sql database, currently an error in the xml file though. I don't know what it is, will update when corrected.

--- internal/internal/LoginScreen.xaml.cs
+++ internal/internal/LoginScreen.xaml.cs
@@ -12,8 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace internal
-{
+namespace internal{
     /// <summary>
     /// Interaction logic for LoginScreen.xaml
     /// </summary>
